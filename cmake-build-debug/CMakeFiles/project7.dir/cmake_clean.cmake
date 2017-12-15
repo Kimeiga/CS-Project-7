43a597be4bce8605517464d97f14f58a19707e2a
Hakan Kimeiga <hak7alp@gmail.com>
2017-12-14 20:11:16 -0800

    init1

---
+++ cmake-build-debug/CMakeFiles/project7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project7.dir/main.cpp.o"
+  "project7.pdb"
+  "project7.exe"
+  "libproject7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
